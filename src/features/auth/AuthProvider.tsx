17fbc43633a4186039595e944b371e9e3c5a4cf7
Wil Johnson <15970051+wilj@users.noreply.github.com>
2020-09-28 04:02:11 +0000

    work in progress

--- src/features/auth/AuthProvider.tsx
+++ src/features/auth/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useMemo } from 'react'
+import React, { useState, useCallback, useMemo, useEffect } from 'react'
 import { KeycloakProvider, KeycloakEventHandler } from '@react-keycloak/web'
 import Keycloak from 'keycloak-js'
 import {
@@ -13,81 +13,90 @@ import { SubscriptionClient } from 'subscriptions-transport-ws'
 import { AuthInfo, User } from './useAuth'
 import { AuthContext, AuthApiContext } from './AuthContext'
 
+export interface AuthClients {
+    keycloak: any
+    urqlClient: any
+    subscriptionClient: any
+}
+export function createAuthClients(baseDomain: string, realm: string, apiDomain?: string) : AuthClients {
+    const clientId = realm
+    const authUrl = `https://auth.${baseDomain}/auth`
+    
+    // TODO FIXME urls need to be props
+    const baseGraphqlUrl = apiDomain ? (apiDomain + `/graphql`) : `://admin.${baseDomain}/graphql`
+    const subscriptionClient = new SubscriptionClient(`wss${baseGraphqlUrl}`, {})
+    
+    const keycloak = Keycloak({  url: authUrl, realm, clientId })
+    
+    const urqlClient = createClient({
+        url: `https${baseGraphqlUrl}`,
+        fetchOptions: () => {
+            return keycloak.token
+                ? {
+                      credentials: 'include',
+                      headers: {
+                          Authorization: `Bearer ${keycloak.token}`,
+                      },
+                  }
+                : {}
+        },
+        exchanges: [
+            debugExchange,
+            /*
+            TODO FIXME make this configurable
+            // cacheExchange is disabled by default
+            cacheExchange,
+            */
+            fetchExchange,
+            subscriptionExchange({
+                forwardSubscription: operation =>
+                    subscriptionClient.request(operation),
+            }),
+        ],
+    })
+    
+    return {keycloak, urqlClient, subscriptionClient}    
+}
+
+
 export type AuthProviderProps = {
     baseDomain: string
     realm: string
+    apiDomain?: string
     busyElement?: JSX.Element
 }
-
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({
     baseDomain,
     realm,
+    apiDomain,
     busyElement,
     children,
 }) => {
     const [auth, setAuth] = useState({} as AuthInfo)  
-
-    const authImpl = useMemo(() => {
-        const clientId = realm
-        const authUrl = `https://auth.${baseDomain}/auth`
-        
-        // TODO FIXME urls need to be props
-        const baseGraphqlUrl = `://admin.${baseDomain}/graphql`
-        const subscriptionClient = new SubscriptionClient(`wss${baseGraphqlUrl}`, {})
-        
-        const keycloak = Keycloak({  url: authUrl, realm, clientId })
-        
-        const urqlClient = createClient({
-            url: `https${baseGraphqlUrl}`,
-            fetchOptions: () => {
-                return keycloak.token
-                    ? {
-                          credentials: 'include',
-                          headers: {
-                              Authorization: `Bearer ${keycloak.token}`,
-                          },
-                      }
-                    : {}
-            },
-            exchanges: [
-                debugExchange,
-                /*
-                TODO FIXME make this configurable
-                // cacheExchange is disabled by default
-                cacheExchange,
-                */
-                fetchExchange,
-                subscriptionExchange({
-                    forwardSubscription: operation =>
-                        subscriptionClient.request(operation),
-                }),
-            ],
-        })
-
-        const handleKeycloakEvent: KeycloakEventHandler = (event, error) => {
-            if (auth.token !== keycloak.token) {
-                setAuth({...auth, token: keycloak.token})
-            }
-        }
-        
-        return {keycloak, handleKeycloakEvent, urqlClient}
-        
-    }, [])
-    const login = useCallback(() => authImpl.keycloak.login({ scope: 'profile' }), [])
+    const clients = useMemo(() => createAuthClients(baseDomain, realm, apiDomain), [baseDomain, realm, apiDomain])
+    const login = useCallback(() => clients.keycloak.login({ scope: 'profile' }), [])
     const logout = useCallback(() => {
                         setAuth(state => ({} as AuthInfo))
-                        authImpl.keycloak.logout()
+                        clients.keycloak.logout()
                     }, [])
     
     const authApi = useMemo(() => ({login, logout, setUser: (user: User) => setAuth({...auth, user } as AuthInfo)}), [])
+    const loading = busyElement || <div className="cyton-loading">Loading...</div> 
+    const handleKeycloakEvent: KeycloakEventHandler = (event, error) => {
+        if (auth.token !== clients.keycloak.token) {
+            setAuth({...auth, token: clients.keycloak.token})
+        }
+    }
+    
+    console.log(`using clients`, clients)
     return (
         <KeycloakProvider
-            onEvent={authImpl.handleKeycloakEvent}
-            LoadingComponent={busyElement || <div className="cyton-loading">Loading...</div>  }
-            keycloak={authImpl.keycloak}
+            onEvent={handleKeycloakEvent}
+            LoadingComponent={ loading }
+            keycloak={clients.keycloak}
         >
-            <UrqlClientProvider value={authImpl.urqlClient}>
+            <UrqlClientProvider value={clients.urqlClient}>
                 <AuthContext.Provider value={auth}>
                     <AuthApiContext.Provider value={authApi}>
                         {children}
