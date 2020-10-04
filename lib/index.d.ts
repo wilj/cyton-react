0a08cdbe0d04519225b53da318b8ee2d1303392d
Wil Johnson <15970051+wilj@users.noreply.github.com>
2020-10-03 20:19:26 -0400

    updated dependencies

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,4 +3,5 @@ export * from './features/auth/AuthProvider';
 export * from './features/auth/AuthRequired';
 export * from './features/state/useLocalStorage';
 export * from 'urql';
+export * from '@react-keycloak/web';
 //# sourceMappingURL=index.d.ts.map
